944bdb9fa8f0fbf39a073e31b62fae0686873ad5
t-sin <shinichi.tanaka45@gmail.com>
2021-05-13 22:02:18 +0900

    Rewrite: Add a invader game module

---
+++ src/invader_game/index.js
@@ -0,0 +1,53 @@
+const game_scene = require('./game_scene/');
+
+const setup = () => {
+  const canvas = document.getElementById('tensho-game');
+  const ctx = canvas.getContext('2d');
+
+  let input = {
+    left: false,
+    right: false,
+    shot: false,
+  };
+  document.addEventListener('keydown', (e) => {
+    switch (e.code) {
+      case 'KeyH':
+        input.left = true;
+        break;
+      case 'KeyL':
+        input.right = true;
+        break;
+      case 'KeyS':
+        input.shot = true;
+        break;
+    }
+  });
+  document.addEventListener('keyup', (e) => {
+    switch (e.code) {
+      case 'KeyH':
+        input.left = false;
+        break;
+      case 'KeyL':
+        input.right = false;
+        break;
+      case 'KeyS':
+        input.shot = false;
+        break;
+    }
+  });
+
+  let game = {
+    canvas: canvas,
+    ctx: ctx,
+    scene: null,
+    debug: true,
+    input: input,
+    conf: null,
+  };
+  game.scene = game_scene.make_game_scene(game);
+  return game;
+};
+
+export const start = () => {
+  setup().scene();
+};
