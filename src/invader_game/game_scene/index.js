9cb6296736844331533be2eea0acafcf7ad5f1fe
t-sin <shinichi.tanaka45@gmail.com>
2021-05-13 23:10:47 +0900

    Rewrite: empty invader-related proc

--- src/invader_game/game_scene/index.js
+++ src/invader_game/game_scene/index.js
@@ -1,6 +1,8 @@
 const constant = require('./constant.js');
 const state = require('./state.js');
 const draw = require('./draw.js');
+//const cannon = require('./cannon.js');
+const invader = require('./invader.js');
 
 export const make_game_scene = (game) => {
   game.conf = constant.config;
@@ -8,6 +10,7 @@ export const make_game_scene = (game) => {
 
   const fn = () => {
     draw.proc(game, game_state);
+    invader.proc(game, game_state);
     //proc(game, game_state);
 
     //window.requestAnimationFrame(fn);
---
+++ src/invader_game/game_scene/invader.js
@@ -0,0 +1,24 @@
+const constant = require('./constant.js');
+
+const iterate_all_invaders = (fn, state) => {
+  for (let j = 0; j < constant.config.rows; j++) {
+    for (let i = 0; i < constant.config.columns; i++) {
+      fn(i, j, state.invaders.array[j][i]);
+    }
+  }
+};
+
+const should_move_down = () => {
+  return false;
+};
+
+const should_animate = () => {
+  return false;
+};
+
+const proc_one_invader = (i, j, invader) => {
+};
+
+export const proc = (game, state) => {
+  iterate_all_invaders(proc_one_invader, state);
+};
