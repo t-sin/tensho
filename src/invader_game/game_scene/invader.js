bc5cf6ba7b50db2c405490b84842f30819f82e77
t-sin <shinichi.tanaka45@gmail.com>
2021-05-14 01:05:05 +0900

    WIP: Rewrite: buggy horizontal turning

--- src/invader_game/game_scene/invader.js
+++ src/invader_game/game_scene/invader.js
@@ -12,7 +12,34 @@ const nth_in_array = (i, j) => {
   return j * constant.config.columns + i;
 };
 
+const make_living_checker = () => {
+  let first_living_idx = -1;
+  let last_living_idx = -1;
 
+  const reset_living_checker = () => {
+    first_living_idx = -1;
+    last_living_idx = -1;
+  };
+
+  const check_living = (i, j, invader) => {
+    const idx = nth_in_array(i, j);
+    if (invader.state.kind == constant.INVADER_ALIVE) {
+      last_living_idx = idx;
+    }
+    if (first_living_idx == -1 && invader.state.kind == constant.INVADER_ALIVE) {
+      first_living_idx = idx;
+    }
+  };
+
+  const get_living_idx = () => {
+    const ret = {
+      first: first_living_idx,
+      last: last_living_idx,
+    };
+    return ret;
+  };
+
+  return { check_living, get_living_idx, reset_living_checker };
 };
 const { reset_living_checker, get_living_idx, check_living } = make_living_checker();
 
@@ -32,6 +59,23 @@ const proc_one_invader = (state, i, j, invader) => {
       invader.x -= constant.config.invaders.speed.x;
     }
   }
+
+  const { first, last } = get_living_idx();
+  const { left, right } = constant.config.edge;
+  const dir_right = state.invaders.direction_right;
+  if (first != -1 && state.invaders.array[first].x < left && !dir_right) {
+    state.invaders.direction_right = true;
+    invader.y += constant.config.invaders.speed.y;
+  }
+  if (last != -1 && state.invaders.array[last].x > right && dir_right) {
+    state.invaders.direction_right = false;
+    invader.y += constant.config.invaders.speed.y;
+  }
+
+  // animate dying invader
+  // hit detection
+
+  check_living(i, j, invader);
 };
 
 export const proc = (game, state) => {
