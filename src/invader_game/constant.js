f879d948ddc7566bdb2222e5a429a1003eb21e10
t-sin <shinichi.tanaka45@gmail.com>
2021-05-13 21:22:14 +0900

    Rewrite: Constants

---
+++ src/invader_game/constant.js
@@ -0,0 +1,42 @@
+export const CANNON_SHOT_DISABLED = 'disable';
+export const CANNON_SHOT_MOVING = 'moving';
+export const CANNON_SHOT_DYING = 'dying';
+export const INVADER_DISABLED = 'disabled';
+export const INVADER_ALIVE = 'alive';
+export const INVADER_DYING = 'dying';
+
+export const config = {
+  rows: 5,
+  columns: 11,
+  edge: {
+    top: 50,
+    bottom: 440,
+    left: 70,
+    right: 570,
+  },
+  cannon: {
+    initial: { x: 320, y: 430 },
+    speed: { x: 5 },
+  },
+  invader: {
+    initial: {
+      topleft: { x: 120, y: 100 },
+      offset: { x: 35, y: 30 },
+    },
+    speed: { x: 5, y: 20 },
+    move_per_frames: 1,
+  },
+  hit: {
+    cannon: {
+      offset: { x: 3, y: 20 },
+      width: 20,
+    },
+    invader: {
+      offset: { x: 3, y: 20 },
+      width: 20,
+    },
+    shot: {
+      offset: { x: 10, y: 20 },
+    },
+  },
+};
