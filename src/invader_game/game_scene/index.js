3001e59c6679d952429732073803b67487fde353
t-sin <shinichi.tanaka45@gmail.com>
2021-05-16 03:20:38 +0900

    Make shot-related module

--- src/invader_game/game_scene/index.js
+++ src/invader_game/game_scene/index.js
@@ -3,6 +3,7 @@ const state = require('./state.js');
 const draw = require('./draw.js');
 const cannon = require('./cannon.js');
 const invader = require('./invader.js');
+const shot = require('./shot.js');
 
 export const make_game_scene = (game) => {
   let game_state = state.setup_state();
@@ -10,6 +11,7 @@ export const make_game_scene = (game) => {
   const frame_fn = () => {
     draw.proc(game, game_state);
     cannon.proc(game, game_state);
+    shot.proc(game, game_state);
     const destroyed = invader.proc(game, game_state);
     game_state.frames++;
 
@@ -22,5 +24,6 @@ export const make_game_scene = (game) => {
       return false;
     }
   };
+
   return frame_fn;
 };
