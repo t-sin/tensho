7d8046d2aa9afad96137aed5de4142e8fd6556ee
t-sin <shinichi.tanaka45@gmail.com>
2021-05-18 00:44:56 +0900

    Add Logo generator

---
+++ to_js_array.py
@@ -0,0 +1,37 @@
+from itertools import groupby
+
+LOGO_PATH = 'tensho_logo.pgm'
+
+if __name__ == '__main__':
+    with open(LOGO_PATH) as f:
+        f.readline()
+        f.readline()
+        w, h = [int(s) for s in f.readline().split(' ')]
+        f.readline()
+        vlis = list(map(lambda line: int(line), f.readlines()))
+
+    image = []
+    for i in range(int(len(vlis) / w)):
+        start = i * w
+        end = start + w
+        image.append(vlis[start:end])
+
+    values = sorted(dict(groupby(map(lambda x: x, vlis))).keys())
+    new_value_dict = dict(zip(values, range(len(vlis))))
+
+    indexed = []
+    for row in image:
+        new_row = []
+        for val in row:
+            new_row.append(new_value_dict[val])
+        indexed.append(new_row)
+
+    #print(indexed)
+
+    print('const logo = {')
+    for row in indexed:
+        print('  ', end='')
+        for v in row:
+            print('{:>2,},'.format(v), end='')
+        print()
+    print(';')
